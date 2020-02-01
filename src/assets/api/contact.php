05ad2870291dbd7e820a873d815585dd6100a3b0
David Bradshaw <david.bradshaw@ibm.com>
2020-02-01 12:40:12 -0500

    fix: resume dates timezone

--- src/assets/api/contact.php
+++ src/assets/api/contact.php
@@ -13,51 +13,32 @@
     $message    = strip_tags(nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES)));
     $result     = array();
 
+    if(empty($name) or empty($email) or empty($message)){
 
-    if(empty($name)){
-
-        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Error!</strong>&nbsp; Name is required.' );
+        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'Name, email and message is required.' );
         echo json_encode($result);
         die;
     }
-
-    if(empty($email)){
-
-        $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Error!</strong>&nbsp; Email is required.' );
-        echo json_encode($result );
-        die;
-    }
-
-    if(empty($message)){
-
-         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Error!</strong>&nbsp; Message body is required.' );
-         echo json_encode($result );
-         die;
-    }
-
-
 
     $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
     $headers .= "Reply-To: ". $email . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-
     $templateTags =  array(
-        '{{subject}}' => $subject,
-        '{{email}}'=>$email,
-        '{{message}}'=>$message,
-        '{{name}}'=>$name,
-        '{{phone}}'=>$phone
-        );
-
+      '{{subject}}' => $subject,
+      '{{email}}'=>$email,
+      '{{message}}'=>$message,
+      '{{name}}'=>$name,
+      '{{phone}}'=>$phone
+    );
 
     $contents = "<table><tbody><tr><td><h2>Subject: " . $subject . "</h2><p>Name: " . $name . "</p><p>E-mail: " . $email . "</p><p>" . $message . "</p></td></tr></tbody></table>";
 
     if ( mail( $to, "[David Website]: " . $subject, $contents, $headers ) ) {
-        $result = array( 'response' => 'success', 'message'=>'<strong>Thank You!</strong>&nbsp; Your email has been delivered.' );
+        $result = array( 'response' => 'success', 'message'=>'Message received.' );
     } else {
-        $result = array( 'response' => 'error', 'message'=>'<strong>Error!</strong>&nbsp; Cann\'t Send Mail.'  );
+        $result = array( 'response' => 'error', 'message'=>'Unable to send message.  Please try again.'  );
     }
 
     echo json_encode( $result );
