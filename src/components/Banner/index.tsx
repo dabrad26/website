b54083d4e346448e20e5be46812a11091ceaa3d2
David Bradshaw <dabrad26@me.com>
2026-01-05 11:34:22 -0800

    fix: update packages and resize on home page banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
 import type { RouteType } from '../../constants/routes';
 import './Banner.scss';
 import { useLocation, useNavigate } from 'react-router';
 
 export default function Banner(props: {currentRoute: RouteType, imageNumber: number}) {
+  const [pageHeight, setPageHeight] = useState(window.innerHeight);
   const location = useLocation();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const resizeEvent = () => {
+      setPageHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', resizeEvent);
+
+    return () => {
+      window.removeEventListener('resize', resizeEvent);
+    };
+  }, []);
 
   const scrollDown = (): void => {
     const image = document.querySelector('#fullscreen-image');
@@ -24,7 +38,7 @@ export default function Banner(props: {currentRoute: RouteType, imageNumber: num
       <section className="banner-area owl-carousel" id="home">
         <div className={`single_slide_banner slide_bg image-${props.imageNumber}`}>
           <div className="container">
-            <div className="row fullscreen d-flex align-items-center" id="fullscreen-image" style={{height: `${window.innerHeight}px`}}>
+            <div className="row fullscreen d-flex align-items-center" id="fullscreen-image" style={{height: `${pageHeight}px`}}>
               <div className="banner-content col-lg-12 justify-content-center">
                 <h1>David Bradshaw</h1>
                 <h3>Software Engineer and UX Designer</h3>
